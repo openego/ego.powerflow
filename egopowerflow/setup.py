dc14ce5174be276e6c86f0a69fd98d8181b045a6
gplessm <guido.plessmann@rl-institut.de>
2016-08-10 14:47:23 +0200

    Add setup.py incl. dependencies

---
+++ egopowerflow/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='egopowerflow',
+      author='openego development group',
+      description='Powerflow analysis based on PyPSA',
+      packages=find_packages(),
+      install_requires=['pandas >= 0.17.0',
+                        'pypsa >= 0.5'
+                        'sqlalchemy',
+                        egoio]
+     )
