59d633e45d92e1c72b255850000acffe26e2174e
Lukas <lukas.wienholt@next-energy.de>
2017-08-10 09:54:46 +0200

    updated storage distribution plot

--- egopowerflow/tools/plot.py
+++ egopowerflow/tools/plot.py
@@ -224,15 +224,17 @@ def storage_distribution(network, filename=None):
         Specify filename
         If not given, figure will be show directly
     """
-    storage_distribution = network.storage_units.p_nom_opt.groupby(network.storage_units.bus).sum()
+    
+    stores = network.storage_units   
+    storage_distribution = network.storage_units.p_nom_opt[stores.index].groupby(network.storage_units.bus).sum().reindex(network.buses.index,fill_value=0.)
 
     fig,ax = plt.subplots(1,1)
     fig.set_size_inches(6,6)
-    
+   
     if sum(storage_distribution) == 0:
          network.plot(bus_sizes=0,ax=ax,title="No extendable storage")
     else:
-         network.plot(bus_sizes=2*storage_distribution,ax=ax,title="Storage distribution")
+         network.plot(bus_sizes=storage_distribution,ax=ax,line_widths=0.1,title="Storage distribution")
     
     if filename is None:
         plt.show()
